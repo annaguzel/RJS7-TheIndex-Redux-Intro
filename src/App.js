6f95239dda5eb11c54344fa7c8d36052c2b03bb2
Annaguzel <you@example.com>
2020-03-18 14:34:07 +0200

    7 passed,5 failed tests

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React, { Component } from "react";
 
-// Data
-import authors from "./data";
-
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 
 class App extends Component {
-  state = {
-    authors: authors,
-    newAuthorId: 5
-  };
-
-  addAuthor = () => {
-    const newAuthor = {
-      id: this.state.newAuthorId,
-      first_name: "Author",
-      last_name: "McAuthorFace",
-      imageUrl:
-        "https://www.netz.de/images/2016-11-21-firefox-focus-header-58cfedd908c25_500_300.jpg",
-      books: [
-        {
-          title: "Anonymous book",
-          color: "mysterious color"
-        }
-      ]
-    };
-    this.setState({
-      authors: this.state.authors.concat(newAuthor),
-      newAuthorId: this.state.newAuthorId + 1
-    });
-  };
 
   render() {
     return (
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <Sidebar addAuthorHandler={this.addAuthor} />
+            <Sidebar />
           </div>
           <div className="content col-10">
-            <AuthorsList authors={this.state.authors} />
+            <AuthorsList />
           </div>
         </div>
       </div>
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,3 +1,6 @@
-/**
- * Add your actions here
- */
+const ADD_AUTHOR="ADD_AUTHOR";
+ export const addAuthor = ()=>{
+     return{
+     type: ADD_AUTHOR
+     };
+ };
